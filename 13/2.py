f9590f58e2b80330b20754461055d32cb5e2f32b
Glazov Nikolay <n.glazov@infinet.ru>
2020-12-30 15:32:57 +0500

    Add 13 (wait calculate)

--- 13/2.py
+++ 13/2.py
@@ -1,24 +1,44 @@
 from itertools import permutations, combinations
 from pprint import pprint
 
-with open("input_1") as f:
+with open("input") as f:
     inp = [l.strip() for l in f.readlines()]
 
 pprint(inp)
 depart = int(inp[0])
-buses = sorted([int(x) for x in inp[1].split(",") if x != "x"])
-print(depart)
-print(buses)
-
-times_buses = []
-for num, bus in enumerate(buses):
-    cur_time = 0
-    cur_times = set()
-    for time in range(1000):
-        cur_time += bus
-        cur_times.add(cur_time)
-first = times_buses[0]
-for i in times_buses[1:]:
-    print(first & i)
+raw_buses = [x for x in inp[1].split(",")]
 
 
+def raw_to_bus(x):
+    if x == "x":
+        return 0
+    return int(x)
+
+
+buses = [raw_to_bus(x) for x in raw_buses]
+
+cur_time = 0
+
+print(f"{buses=}")
+
+while True:
+    cur_time += buses[0]
+    print(f"{cur_time=}")
+    for num, bus in enumerate(buses[1:]):
+        # print(f"{bus=}")
+        # print(f"{num=}")
+        if bus == 0:
+            continue
+        if (cur_time + num + 1) % bus != 0:
+            break
+    else:
+        print("got break")
+        break
+print(f"{cur_time=}")
+# for num, bus in enumerate(buses[1:]):
+#     if bus == 0:
+#         continue
+#     print(f"{bus}")
+#     print(f"{num}")
+#     print(cur_time + num)
+#     print(cur_time + num % bus)
