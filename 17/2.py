76affedac82159544707f1a79b5d524b5a026635
nigredon1991 <nigredon1991@gmail.com>
2021-06-14 06:14:24 +0500

    Add 17 1

--- 17/2.py
+++ 17/2.py
@@ -2,38 +2,39 @@ from itertools import product
 
 import numpy as np
 
-with open("input_big") as f:
+with open("input") as f:
     inp = [line.strip() for line in f.readlines()]
 print(inp)
 
 # 1 # - active
 # 0 . - inactive
 
-elem_to_check = list(product([0, 1, -1], [0, 1, -1], [0, 1, -1]))
-elem_to_check.remove((0, 0, 0))
+elem_to_check = list(product([0, 1, -1], [0, 1, -1], [0, 1, -1], [0, 1, -1]))
+elem_to_check.remove((0, 0, 0, 0))
 
 
-def get_elem_alive(i, j, k, array):
+def get_elem_alive(i, j, k, w, array):
     count_alive_around = 0
     for elem in elem_to_check:
         if (
             i + elem[0] > (len(array) - 1) or i + elem[0] < 0
             or j + elem[1] > (len(array) - 1) or j + elem[1] < 0
             or k + elem[2] > (len(array) - 1) or k + elem[2] < 0
+            or w + elem[3] > (len(array) - 1) or w + elem[2] < 0
         ):
             continue
-        if array[i + elem[0]][j + elem[1]][k + elem[2]] == 1:
+        if array[i + elem[0]][j + elem[1]][k + elem[2]][w + elem[3]] == 1:
             count_alive_around += 1
-    if array[i][j][k] == 1 and 2 <= count_alive_around <= 3:
+    if array[i][j][k][w] == 1 and 2 <= count_alive_around <= 3:
         return 1
-    if array[i][j][k] == 0 and count_alive_around == 3:
+    if array[i][j][k][w] == 0 and count_alive_around == 3:
         return 1
     return 0
 
 
-n = 50
+n = 16
 
-area = np.zeros((n, n, n))
+area = np.zeros((n, n, n, n))
 
 k = len(inp)
 
@@ -42,9 +43,10 @@ for num_i, i in enumerate(inp):
         area[n // 2][n // 2 - k + num_i][n // 2 - k // 2 + num_j] = 0 if j == "." else 1
 
 for _ in range(6):
-    area_new = np.zeros((n, n, n))
-    for elem in product(range(n), range(n), range(n)):
-        area_new[elem[0]][elem[1]][elem[2]] = get_elem_alive(elem[0], elem[1], elem[2], area)
+    print("Step")
+    area_new = np.zeros((n, n, n, n))
+    for elem in product(range(n), range(n), range(n), range(n)):
+        area_new[elem[0]][elem[1]][elem[2]][elem[3]] = get_elem_alive(elem[0], elem[1], elem[2], elem[3], area)
     area = area_new
 
 count_out = 0
